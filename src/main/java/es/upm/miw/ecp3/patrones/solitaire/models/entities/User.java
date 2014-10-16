3572fe6642782e901ee205df0f0326dab13b8c3f
bl0823 <bl0823@192.168.7.20>
2014-10-16 11:12:13 +0200

    Añadir entities User

--- src/main/java/es/upm/miw/ecp3/patrones/solitaire/models/entities/User.java
+++ src/main/java/es/upm/miw/ecp3/patrones/solitaire/models/entities/User.java
@@ -1,5 +1,11 @@
 package es.upm.miw.ecp3.patrones.solitaire.models.entities;
 
 public class User {
+	
+	private Integer id;
+	private String name;
+	private String password;
+	private int level;
+
 
 }
